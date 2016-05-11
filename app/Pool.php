d5c704686e2e5eeb743c569ecec1a714efa782e1
vagrant <vagrant@homestead>
2016-05-11 22:28:44 +0000

    Pool dummy project with angular and laravel 5.2

---
+++ app/Pool.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pool extends Model
+{
+
+	protected $fillable = ['title'];
+
+    public function poolOptions()
+    {
+        return $this->hasMany('App\PoolOptions');
+    }
+}
